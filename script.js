290505d2c3c6f0d11bd759f609ae36d1a1a1e11a
Francisco Solis <franciscosoliscss@gmail.com>
2020-07-18 18:40:49 -0500

    Acortando el codigo a una sola funcion para la interactividad del boton

--- script.js
+++ script.js
@@ -1,22 +1,33 @@
 
 const lightButton = document.querySelector('.light-mode');
 const darkButton = document.querySelector('.dark-mode');
+let mode = false;
 // console.log(lightButton);
 // console.log(darkButton);
 
-lightButton.addEventListener('click', changeDarkMode);
-darkButton.addEventListener('click', changeLightMode);
+lightButton.addEventListener('click', changeMode);
+darkButton.addEventListener('click', changeMode);
 
-function changeDarkMode(){
+function changeMode(){
     lightButton.classList.toggle('lm-is-active');
     darkButton.classList.toggle('dm-is-active');
-
+    
     // Otra alternativa
     // lightButton.style.display = "none";
     // darkButton.style.display = "initial";
+
+    if(!mode){
+        darkMode();
+        mode = true;
+    }
+    else{
+        lightMode();
+        mode = false;
+    }
+    
 }
 
-function changeLightMode(){
-    lightButton.classList.toggle('lm-is-active');
-    darkButton.classList.toggle('dm-is-active');
+
+function darkMode(){
+
 }
