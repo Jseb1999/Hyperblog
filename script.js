e3f13150a062b34b868c220858d0da4da3b3ae01
Francisco Solis <franciscosoliscss@gmail.com>
2020-07-18 18:34:57 -0500

    Interactividad del boton agregada

--- script.js
+++ script.js
@@ -3,3 +3,20 @@ const lightButton = document.querySelector('.light-mode');
 const darkButton = document.querySelector('.dark-mode');
 // console.log(lightButton);
 // console.log(darkButton);
+
+lightButton.addEventListener('click', changeDarkMode);
+darkButton.addEventListener('click', changeLightMode);
+
+function changeDarkMode(){
+    lightButton.classList.toggle('lm-is-active');
+    darkButton.classList.toggle('dm-is-active');
+
+    // Otra alternativa
+    // lightButton.style.display = "none";
+    // darkButton.style.display = "initial";
+}
+
+function changeLightMode(){
+    lightButton.classList.toggle('lm-is-active');
+    darkButton.classList.toggle('dm-is-active');
+}
