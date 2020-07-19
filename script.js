6da262c51974563e5862d32abd25f07c3b112138
Francisco Solis <franciscosoliscss@gmail.com>
2020-07-18 19:04:36 -0500

    Agregando clases para estilizar en el index, estilos a dark mode, container y a footer, modo dark terminado en js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 
 const lightButton = document.querySelector('.light-mode');
 const darkButton = document.querySelector('.dark-mode');
+const container = document.getElementById('container');
+const body = document.getElementById('body');
 let mode = false;
 // console.log(lightButton);
 // console.log(darkButton);
@@ -12,22 +14,20 @@ function changeMode(){
     lightButton.classList.toggle('lm-is-active');
     darkButton.classList.toggle('dm-is-active');
     
+
+    container.classList.toggle('container-dark');
+    body.classList.toggle('body-dark');
     // Otra alternativa
     // lightButton.style.display = "none";
     // darkButton.style.display = "initial";
 
-    if(!mode){
-        darkMode();
-        mode = true;
-    }
-    else{
-        lightMode();
-        mode = false;
-    }
+    // if(!mode){
+    //     darkMode();
+    //     mode = true;
+    // }
+    // else{
+    //     lightMode();
+    //     mode = false;
+    // }
     
-}
-
-
-function darkMode(){
-
 }
